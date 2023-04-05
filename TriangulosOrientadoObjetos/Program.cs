584e115b545c5090d350c1e43d3b41a93d69dd50
Paola Oliveira <paolaoliveira.dev@gmail.com>
2023-04-05 17:41:11 -0300

    Add project files.

---
+++ TriangulosOrientadoObjetos/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace TriangulosOrientadoObjetos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Clear();
+
+                Triangulo triangulo = new Triangulo();
+
+                triangulo.MensagemColorida("Digite os valores dos lados do triângulo:", ConsoleColor.DarkCyan);
+                Console.WriteLine();
+
+                triangulo.MensagemColorida("\nLado 1: ", ConsoleColor.DarkCyan);
+                triangulo.lado1 = double.Parse(Console.ReadLine());
+
+                triangulo.MensagemColorida("\nLado 2: ", ConsoleColor.DarkCyan);
+                triangulo.lado2 = double.Parse(Console.ReadLine());
+
+                triangulo.MensagemColorida("\nLado 3: ", ConsoleColor.DarkCyan);
+                triangulo.lado3 = double.Parse(Console.ReadLine());
+
+                Console.WriteLine();
+                triangulo.VerificarTriangulo();
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ TriangulosOrientadoObjetos/Triangulo.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriangulosOrientadoObjetos
+{
+    internal class Triangulo
+    {
+        public double lado1, lado2, lado3;
+        
+        public void VerificarTriangulo()
+        {
+            if (lado1 <= 0 || lado2 <= 0 || lado3 <= 0)
+            {
+                MensagemColorida("Os lados do triângulo devem ser maiores que zero!", ConsoleColor.Red);
+            }
+            else if (lado1 + lado2 <= lado3 || lado1 + lado3 <= lado2 || lado2 + lado3 <= lado1)
+            {
+                MensagemColorida("Triângulo Inválido.", ConsoleColor.Red);
+            }
+            else
+            {
+                // Verifica a classificação do triângulo inserido
+                if (lado1 == lado2 && lado1 == lado3 && lado2 == lado3)
+                {
+                    MensagemColorida("Triângulo Equilátero.", ConsoleColor.Cyan);
+                }
+                else if (lado1 != lado2 && lado1 != lado3 && lado2 != lado3)
+                {
+                    MensagemColorida("Triângulo Escaleno.", ConsoleColor.Cyan);
+                }
+                else
+                {
+                    MensagemColorida("Triângulo Isósceles.", ConsoleColor.Cyan);
+                }
+            }
+        }
+
+        public void MensagemColorida(string mensagem, ConsoleColor cor)
+        {
+            Console.ForegroundColor = cor;
+            Console.Write(mensagem);
+            Console.ResetColor();
+        }
+    }
+}
